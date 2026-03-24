78b24619915e8d615c481c1642f993f337ec5715
swethaharinisuresh-debug <swethaharinisuresh@gmail.com>
2026-03-24 08:59:06 +0530

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,139 @@
+#for i in range (1,51):
+ #   print (i)
+
+#for number in range(2,101,2):
+ #   print(number)
+
+#for i in range(1,101,2):
+ #   print(i)
+
+#num=7
+#for i in range (1,11):
+  #  print(f"{num}*{i}={num*i}")
+
+#total=0
+#for i in range(1,101):
+ #   total+=i
+#print(f"the sum of numbers from 1 to 100 is: {total}")
+
+
+
+
+#for i in range(50,0,-1):
+ #   print(i)
+
+ # count=0
+#for i in range (1,101):
+ #   if i % 3 == 0:
+  #      count += 1
+#print  (count)
+
+#for i in range(1,11):
+ #   print(f"{i}{i**2}")
+
+#for i in range (1,11):
+ #   cube = i ** 3
+  #  print(f"cube of {i} is (cube)")
+
+#N=int(input("enter an int: "))
+#print(f"numbers from 1 to {N} are:")
+#for i in range (1,N+1):
+ #   print(i)
+
+#num=1
+#while num<=20:
+ #   print(num)
+  #  num+=1
+
+
+#print("enter the num: ")
+
+#num=int(input())
+#fact=1
+#i=1
+#while i <=1:
+ #   fact = fact *i
+  #  i=i+1
+
+
+#num=1234
+#reversed_num=1
+#while num !=0:
+ ### num//=10
+#print("reversed number:" + set (reversed_num))
+
+
+#num=12345
+#count=0
+#while num != 0:
+  #  num//=10
+ #   count += 1
+#print("number of digits: " + str(count
+
+#user_input=""
+#while user_input.lower () !="stop":
+ #   user_input= input ("please enter something(type 'stop' to end): ")
+  #  if user_input.lower() !="stop":
+   #     print (f"you entered: {user_input}")
+#print("loop stopped.")
+
+#rows=4
+#for i in range(1,rows+1):
+#    for j in range(i):
+ #       print("*",end="")
+#p
+
+#for i in range(1,6):
+ #   print(f"multiplication table for {i}:")
+#for j in range(1,11):
+ #   print(f"{i} * {j} = {i*j}") 
+  #  print (i*j,end="")
+
+#for i in range(3):
+ ##w     print()
+#my_string="hello,world!"
+#char_count=len(my_string)
+#print ("number of character:",char_count)
+
+name="swetha"
+#print(name[0:5 ])
+#print(name[3: ])
+#print(name[-1])
+#print(name[0:5:2])
+#print(name[1:5])
+
+
+lst=[1,2,3,4,5,6]
+#total_sum=sum(lst)
+#print(sum(lst))
+#maximum_value=max(lst)
+#print(maximum_value)
+
+#minimum_value=min(lst)
+#print(minimum_value)
+
+#total_elements=len(lst)
+#print(len(lst))
+
+#element_to_check="1"
+#if element_to_check in lst:
+ #   print("yes")
+#else:
+ #   print("no")
+#print(1)
+
+#lst.append("7")
+#lst.append("8")
+#lst.append("9")
+#print(lst)
+
+#lst.insert(2,"9")
+#print(lst)
+
+#lst.remove(2)
+#print(lst)
+
+#lst1=lst[::-1]
+#print(lst1)
+
+
